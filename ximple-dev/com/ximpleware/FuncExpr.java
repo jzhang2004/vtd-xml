cd28a62899a7435b9d9fcfafeba753cb2e7712d8
jzhang2004 <jzhang2004>
2005-09-02 03:20:20 +0000

    Initial check in of FuncExpr

---
+++ ximple-dev/com/ximpleware/FuncExpr.java
@@ -0,0 +1,197 @@
+/* 
+ * Copyright (C) 2002-2004 XimpleWare, info@ximpleware.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+package com.ximpleware;
+import com.ximpleware.xpath.Alist;
+import com.ximpleware.xpath.Expr;
+import com.ximpleware.xpath.FuncName;
+import com.ximpleware.xpath.UnsupportedException;
+import com.ximpleware.xpath.XPathEvalException;
+
+public class FuncExpr extends Expr{
+
+	public Alist argumentList;
+	public int opCode;
+	boolean isNumerical;
+	int a;
+	int argCount(){
+		Alist temp = argumentList;
+		int count = 0;
+		while(temp!=null){
+			count++;
+			temp = temp.next;
+		}
+		return count;
+	}
+	public FuncExpr(int oc , Alist list){
+		a = 1;
+	  opCode = oc;
+	  argumentList = list;
+	  switch(opCode){
+			case FuncName.LAST: 			isNumerical = true;break;
+			case FuncName.POSITION: 		isNumerical = true;break;
+			case FuncName.COUNT: 			isNumerical = true;break;
+			case FuncName.LOCAL_NAME: 		isNumerical = false;break;
+			case FuncName.NAMESPACE_URI: 		isNumerical = false;break;
+			case FuncName.NAME: 			isNumerical = false;break;
+			case FuncName.STRING: 			isNumerical = false;break;
+			case FuncName.CONCAT: 			isNumerical = false;break;
+			case FuncName.STARTS_WITH:		isNumerical = false;break;
+			case FuncName.CONTAINS: 		isNumerical = false;break;
+			case FuncName.SUBSTRING_BEFORE: 	isNumerical = false;break;
+			case FuncName.SUBSTRING_AFTER: 		isNumerical = false;break;
+			case FuncName.SUBSTRING: 		isNumerical = false;break;
+			case FuncName.STRING_LENGTH: 		isNumerical = true;break;
+			case FuncName.NORMALIZE_SPACE: 		isNumerical = false;break;
+			case FuncName.TRANSLATE:	 	isNumerical = false;break;
+			case FuncName.BOOLEAN: 			isNumerical = false;break;
+			case FuncName.NOT: 			isNumerical = false;break;
+			case FuncName.TRUE: 			isNumerical = false;break;
+			case FuncName.FALSE: 			isNumerical = false;break;
+			case FuncName.LANG: 			isNumerical = false;break;
+			case FuncName.NUMBER:			isNumerical = true;break;
+			case FuncName.SUM: 			isNumerical = true;break;
+			case FuncName.FLOOR: 			isNumerical = true;break;
+			case FuncName.CEILING: 			isNumerical = true;break;
+			default:			isNumerical = true;;
+	  }	  
+	}
+
+	public String toString(){
+	  if (argumentList == null)
+		  return fname()+" ("+")";
+	  return fname()+" ("+argumentList +")";
+	}
+	public String evalString(VTDNav vn) throws UnsupportedException{
+	  switch(opCode){
+			case FuncName.LOCAL_NAME: 			
+			case FuncName.NAMESPACE_URI: 	
+			case FuncName.NAME: 			
+			case FuncName.SUBSTRING_BEFORE:		
+			case FuncName.SUBSTRING_AFTER: 		
+			case FuncName.SUBSTRING: 		
+			case FuncName.TRANSLATE: 	
+			case FuncName.NORMALIZE_SPACE:
+			default: throw new UnsupportedException("Some functions are not supported");
+	  }
+	}	
+	public double evalNumber(VTDNav vn) throws UnsupportedException{
+	  switch(opCode){
+			case FuncName.LAST:  throw new UnsupportedException
+									("Some functions are not supported"); 			
+			case FuncName.POSITION: return a++;
+			case FuncName.COUNT: 	if (argCount()!=1)
+										throw new IllegalArgumentException
+											("Count()'s argument count is invalid");
+									vn.push2();
+									try{
+										a = 0;
+										while(argumentList.e.evalNodeSet(vn)!=-1){
+											a ++;
+										}
+										argumentList.e.reset(vn);
+										vn.pop2();
+										return a;
+									}catch(Exception e){
+										argumentList.e.reset(vn);
+										vn.pop2();
+										throw new IllegalArgumentException
+										("Count()'s argument must be a node set");
+									}
+			case FuncName.NUMBER:   if (argCount()!=1)
+										throw new IllegalArgumentException
+										("number()'s argument count is invalid");
+									return argumentList.e.evalNumber(vn);
+			case FuncName.SUM: 		
+			case FuncName.FLOOR: 	
+			case FuncName.CEILING:
+			case FuncName.STRING_LENGTH:
+			case FuncName.ROUND: 
+			default: throw new UnsupportedException("Some functions are not supported");
+	  }
+	}
+
+	public int evalNodeSet(VTDNav vn) throws XPathEvalException{
+	  throw new XPathEvalException(" Function Expr can't eval to node set ");
+	}
+	
+	public boolean evalBoolean(VTDNav vn){
+	  	  switch(opCode){
+			case FuncName.TRUE: if (argCount()!=0){
+									throw new IllegalArgumentException("true() doesn't take any argument");
+								}
+								return true;			
+			case FuncName.FALSE:if (argCount()!=0){
+									throw new IllegalArgumentException("false() doesn't take any argument");
+								}
+								return false;	
+			case FuncName.BOOLEAN: if (argCount()!=1){
+										throw new IllegalArgumentException("boolean() doesn't take any argument");
+								   }
+									return argumentList.e.evalBoolean(vn);	
+			case FuncName.NOT:	if (argCount()!=1){
+										throw new IllegalArgumentException("not() doesn't take any argument");
+			   					}
+								return !argumentList.e.evalBoolean(vn);
+			case FuncName.CONTAINS:	
+			default: throw new UnsupportedException("Some functions are not supported");
+		  }
+	}
+	
+	public void reset(VTDNav vn){
+		if (argumentList!=null)
+			argumentList.reset(vn);
+	}
+
+	public String fname(){
+		switch(opCode){
+			case FuncName.LAST: 			return "last";
+			case FuncName.POSITION: 		return "position";
+			case FuncName.COUNT: 			return "count";
+			case FuncName.LOCAL_NAME: 		return "local-name";
+			case FuncName.NAMESPACE_URI: 		return "namespace-uri";
+			case FuncName.NAME: 			return "name";
+			case FuncName.STRING: 			return "string";
+			case FuncName.CONCAT: 			return "concat";
+			case FuncName.STARTS_WITH:		return "starts-with";
+			case FuncName.CONTAINS: 		return "contains";
+			case FuncName.SUBSTRING_BEFORE: 	return "substring_before";
+			case FuncName.SUBSTRING_AFTER: 		return "substring_after";
+			case FuncName.SUBSTRING: 		return "substring";
+			case FuncName.STRING_LENGTH: 		return "string-length";
+			case FuncName.NORMALIZE_SPACE: 		return "normalize-space";
+			case FuncName.TRANSLATE:	 	return "translate";
+			case FuncName.BOOLEAN: 			return "boolean";
+			case FuncName.NOT: 			return "not";
+			case FuncName.TRUE: 			return "true";
+			case FuncName.FALSE: 			return "false";
+			case FuncName.LANG: 			return "lang";
+			case FuncName.NUMBER:			return "number";
+			case FuncName.SUM: 			return "sum";
+			case FuncName.FLOOR: 			return "floor";
+			case FuncName.CEILING: 			return "ceiling";
+			default:			return "round";
+		}
+	}
+	public boolean  isNodeSet(){
+		return false;
+	}
+
+	public boolean  isNumerical(){
+		return isNumerical;
+	}
+}
