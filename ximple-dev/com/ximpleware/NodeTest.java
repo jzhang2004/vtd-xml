b53944d41d95a76913b5b3f0d295ac85411eef9b
jzhang2004 <jzhang2004>
2005-09-09 23:38:15 +0000

    removed some redundant imports

--- ximple-dev/com/ximpleware/NodeTest.java
+++ ximple-dev/com/ximpleware/NodeTest.java
@@ -16,7 +16,6 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 package com.ximpleware;
-import com.ximpleware.*;
 import com.ximpleware.xpath.LocationPathNode;
 /**
  * This class is used within LocationPathExpr to represent 
