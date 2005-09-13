e96944e2dd147907bfb9514ae00754c2b336344e
jzhang2004 <jzhang2004>
2005-09-13 06:24:34 +0000

    initial check in unaryExpr

---
+++ ximple-dev_c/vtd-xml/literalExpr.c
@@ -0,0 +1,118 @@
+/* 
+* Copyright (C) 2002-2004 XimpleWare, info@ximpleware.com
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+#include "xpath1.h"
+
+literalExpr *createLiteralExpr(UCSChar *st){
+	exception e;
+	literalExpr *l = (literalExpr*) malloc(sizeof(literalExpr));
+	if (l==NULL){
+		e.et = out_of_mem;
+		e.msg = "literalExpr allocation failed ";
+		Throw e;
+	}
+	l->freeExpr = &freeLiteralExpr;
+	l->evalBoolean = &evalBoolean_le;
+	l->evalNodeSet = &evalNodeSet_le;
+	l->evalNumber  = &evalNumber_le;
+	l->evalString  = &evalString_le;
+	l->isNumerical = &isNumerical_le;
+	l->isBoolean = &isBoolean_le;
+	l->isString =  &isString_le;
+	l->isNodeSet = &isNodeSet_le;
+	l->requireContextSize = &requireContextSize_le;
+	l->setContextSize = &setContextSize_le;
+	l->setPosition = &setPosition_le;
+	l->reset = &reset_le;
+	l->toString = &toString_le;
+	l->s= st;
+
+	
+	return l;
+
+}
+
+void freeLiteralExpr(literalExpr *le){
+	free(le->s);
+	free(le);
+}
+
+int	evalNodeSet_le (literalExpr *le,VTDNav *vn){
+	exception e;
+	e.et = xpath_eval_exception;
+	e.msg = "LiteralExpr can't eval to a node set!";
+	Throw e;	
+}
+
+double	evalNumber_le (literalExpr *le,VTDNav *vn){
+	double d  = 0;
+	UCSChar *temp;
+	double result = wcstod(le->s,&temp);
+	while(*temp!=0){
+		if ( *temp == L' ' 
+			|| *temp == L'\n'
+			|| *temp == L'\t'
+			|| *temp == L'\r'){ 			
+				temp++;
+			}
+		else 
+			return d/d; //NaN
+	}
+	return result;
+}
+
+UCSChar* evalString_le  (literalExpr *le,VTDNav *vn){
+	return le->s;
+}
+
+Boolean evalBoolean_le (literalExpr *le,VTDNav *vn){
+	size_t len = wcslen(le->s);
+	return len != 0;
+}
+
+Boolean isBoolean_le (literalExpr *le){
+	return FALSE;
+}
+
+Boolean isNumerical_le (literalExpr *le){
+	return FALSE;
+}
+
+Boolean isString_le (literalExpr *le){
+	return TRUE;
+}
+
+Boolean isNodeSet_le (literalExpr *le){
+	return FALSE;
+}
+
+Boolean requireContextSize_le(literalExpr *le){
+	return FALSE;
+}
+
+void	reset_le(literalExpr *le, VTDNav *vn){
+}
+
+void	setContextSize_le(literalExpr *le,int s){
+}
+
+void	setPosition_le(literalExpr *le,int pos){
+}
+
+void    toString_le(literalExpr *le, UCSChar* string){
+	wprintf(L"\"%s\"",le->s);
+}
---
+++ ximple-dev_c/vtd-xml/unaryExpr.c
@@ -0,0 +1,125 @@
+/* 
+* Copyright (C) 2002-2004 XimpleWare, info@ximpleware.com
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+#include "xpath1.h"
+
+unaryExpr *createUnaryExpr(opType op, expr *e1){
+	exception e;
+	unaryExpr* ue = (unaryExpr *)malloc(sizeof(unaryExpr));
+	if (ue == NULL){
+		//e1->freeExpr(e1);
+		e.et = out_of_mem;
+		e.msg = "unaryExpr allocation failed ";
+		Throw e;
+	}
+	ue->freeExpr = &freeUnaryExpr;
+	ue->evalBoolean = &evalBoolean_ue;
+	ue->evalNodeSet = &evalNodeSet_ue;
+	ue->evalNumber  = &evalNumber_ue;
+	ue->evalString  = &evalString_ue;
+	ue->isNumerical = &isNumerical_ue;
+	ue->isBoolean = &isBoolean_ue;
+	ue->isString =  &isString_ue;
+	ue->isNodeSet = &isNodeSet_ue;
+	ue->requireContextSize = &requireContextSize_ue;
+	ue->setContextSize = &setContextSize_ue;
+	ue->setPosition = &setPosition_ue;
+	ue->reset = &reset_ue;
+	ue->toString = &toString_ue;
+	ue->e = e1;
+	ue->op = op;
+
+	return ue;
+}
+void freeUnaryExpr(unaryExpr *ue){
+	if (ue!=NULL && ue->e != NULL)
+		(ue->e->freeExpr)(ue->e);
+	free(ue);
+}
+
+int	evalNodeSet_ue (unaryExpr *ue,VTDNav *vn){
+	exception e;
+	e.et = xpath_eval_exception;
+	e.msg = "unaryExpr can't eval to a node set!";
+	Throw e;
+}
+
+double	evalNumber_ue (unaryExpr *ue,VTDNav *vn){
+	return -1*(ue->e->evalNumber)(ue->e, vn);
+}
+
+UCSChar* evalString_ue  (unaryExpr *ue,VTDNav *vn){
+	exception e;
+	UCSChar *string1 = (ue->e->evalString)(ue->e,vn);
+	size_t len = wcslen(string1);
+	UCSChar *string = (UCSChar*) malloc((len+1)*sizeof(UCSChar));
+	if (string == NULL) {
+			e.et = out_of_mem;
+			e.msg = "allocate string failed in funcExpr's evalString()";
+			Throw e;
+	}
+#ifdef VC
+	swprintf(string,L"-%s",string1);
+#endif
+
+#ifdef GCC
+	wsprintf(string,L"-%s",string1);
+#endif
+	free(string1);
+	return string;
+}
+
+Boolean evalBoolean_ue (unaryExpr *ue,VTDNav *vn){
+	return (ue->e->evalBoolean)(ue->e, vn);
+}
+
+Boolean isBoolean_ue (unaryExpr *ue){
+	return FALSE;
+}
+
+Boolean isNumerical_ue (unaryExpr *ue){
+	return TRUE;
+}
+
+Boolean isString_ue (unaryExpr *ue){
+	return FALSE;
+}
+
+Boolean isNodeSet_ue (unaryExpr *ue){
+	return FALSE;
+}
+
+Boolean requireContextSize_ue(unaryExpr *ue){
+	return (ue->e->requireContextSize)(ue->e);
+}
+
+void	reset_ue(unaryExpr *ue, VTDNav *vn){
+	(ue->e->reset)(ue->e, vn);
+}
+
+void	setContextSize_ue(unaryExpr *ue,int s){
+	(ue->e->setContextSize)(ue->e, s);
+}
+
+void	setPosition_ue(unaryExpr *ue,int pos){
+	(ue->e->setPosition)(ue->e, pos);
+}
+
+void    toString_ue(unaryExpr *ue, UCSChar* string){
+	wprintf(L"-");
+	(ue->e->toString)(ue->e, string);
+}
