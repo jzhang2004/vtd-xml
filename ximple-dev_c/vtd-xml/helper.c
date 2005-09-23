7a13232b34cd9574c45c4e48f52f5d13e21170fc
jzhang2004 <jzhang2004>
2005-09-23 04:15:08 +0000

    initial checkin

---
+++ ximple-dev_c/vtd-xml/helper.c
@@ -0,0 +1,61 @@
+/* 
+ * Copyright (C) 2002-2004 XimpleWare, info@ximpleware.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+#include "helper.h"
+
+
+void addObj(void *o){
+	exception e;
+	
+	if (objList ==NULL){
+		objList = ptr1 = (struct obj *)malloc(sizeof(struct obj));
+		if (ptr1 == NULL){
+			e.et = out_of_mem;
+			Throw e;
+		} 		
+	} else {
+		ptr1->next = malloc(sizeof(struct obj));	
+		if (ptr1->next == NULL){
+			e.et = out_of_mem;
+			Throw e;
+		} 	
+		ptr1 = ptr1->next;
+	}
+	ptr1->object = o;
+	ptr1->next = NULL;
+	
+}
+void freeAllObj(){
+	ptr1 = objList;
+	while(ptr1!=NULL){
+		free(ptr1->object);
+		ptr2 = ptr1->next;
+		free(ptr1);
+		ptr1 = ptr2;
+	}
+	ptr1 = ptr2 = objList = NULL;
+}
+void resetObj(){
+	ptr1 = objList;
+	while(ptr1!=NULL){
+		//free(ptr1->object);
+		ptr2 = ptr1->next;
+		free(ptr1);
+		ptr1 = ptr2;
+	}
+	ptr1 = ptr2 = objList = NULL;
+}
---
+++ ximple-dev_c/vtd-xml/helper.h
@@ -0,0 +1,35 @@
+/* 
+* Copyright (C) 2002-2005 XimpleWare, info@ximpleware.com
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+#ifndef HELPER_H
+#define HELPER_H
+
+#include "customTypes.h"
+#include <stdlib.h>
+struct obj {
+   void* object;
+   struct obj *next;
+} ;
+struct obj *objList;
+struct obj *ptr1;
+struct obj *ptr2;
+   
+void addObj(void *obj); // add to a global linked list during yyparse, to prevent mem leak
+void freeAllObj();  // if there is anything wrong during parsing
+void resetObj(); // this function is called if yyparse return 0
+
+#endif
