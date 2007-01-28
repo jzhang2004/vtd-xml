e978839b4cc7df9cea6794b16079b01eb5035c26
jzhang2004 <jzhang2004>
2007-01-28 04:42:11 +0000

    copy right change to 2007

--- ximple-dev/com/ximpleware/IObjectBuffer.java
+++ ximple-dev/com/ximpleware/IObjectBuffer.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2002-2006 XimpleWare, info@ximpleware.com
+ * Copyright (C) 2002-2007 XimpleWare, info@ximpleware.com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@ package com.ximpleware;
 /**
  * Abstract object pointer storage interface
  */
-public interface IObjectBuffer {
+interface IObjectBuffer {
     Object objectAt(int i);
     /**
      * Modify the Object pointer at index to value val.     * 
--- ximple-dev/com/ximpleware/IReader.java
+++ ximple-dev/com/ximpleware/IReader.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2002-2006 XimpleWare, info@ximpleware.com
+ * Copyright (C) 2002-2007 XimpleWare, info@ximpleware.com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@ package com.ximpleware;
  * Abstract Reader Class decodes underlying raw bytes.
  * Creation date: (3/5/04 7:18:57 PM)
  */
-public interface IReader {
+interface IReader {
     /**
      * Get next available Char from the buffer.
      * Creation date: (3/5/04 7:39:44 PM)
