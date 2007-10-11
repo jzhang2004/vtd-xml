c3ba236a495b0d3ed710d0588bf9b8ac23be9c84
djmay <djmay>
2007-10-11 03:35:00 +0000

    Added static getter and packing methods

--- vtd-xml-HUGE/java/com/ximpleware/VTDBits.java
+++ vtd-xml-HUGE/java/com/ximpleware/VTDBits.java
@@ -1,5 +1,7 @@
 package com.ximpleware;
 
+import com.ximpleware.enums.TokenType;
+
 /**
  * Created by IntelliJ IDEA.
  * User: dmay
@@ -56,6 +58,52 @@ public class VTDBits
     public final static long TOKEN_MASK = (maskstart >> TOKEN_BIT_SIZE -1) >>> 63-TOKEN_START_BIT;
     public final static long TOKEN_MAX_VALUE = (1 << TOKEN_BIT_SIZE) -1;
     public final static int TOKEN_SHIFT_BITS = QNAME_SHIFT_BITS;
+
+    public static long getOffset(long vtd)
+    {
+        return (OFFSET_MASK & vtd) >> OFFSET_SHIFT_BITS;
+    }
+
+    public static long getQName(long vtd)
+    {
+        return (QNAME_MASK & vtd) >> QNAME_SHIFT_BITS;
+    }
+
+    public static long getPrefix(long vtd)
+    {
+        return (PREFIX_MASK & vtd) >> PREFIX_SHIFT_BITS;
+    }
+
+    public static long getDepth(long vtd)
+    {
+        return (DEPTH_MASK & vtd) >> DEPTH_SHIFT_BITS;
+    }
+
+    public static TokenType getType(long vtd)
+    {
+        return TokenType.valueOf((int)((TYPE_MASK & vtd) >> TYPE_SHIFT_BITS));
+    }
+
+    public static long packns(long offset, long qname, long prefix, long depth, TokenType type)
+    {
+        offset = offset << OFFSET_SHIFT_BITS;
+        qname = qname << QNAME_SHIFT_BITS;
+        prefix = prefix << PREFIX_SHIFT_BITS;
+        depth = depth << DEPTH_SHIFT_BITS;
+        long typel = (long)(type.getIntValue()) << TYPE_SHIFT_BITS;
+
+        return offset | qname | prefix | depth | typel; 
+    }
+
+    public static long pack(long offset, long qname, long depth, TokenType type)
+    {
+        offset = offset << OFFSET_SHIFT_BITS;
+        qname = qname << QNAME_SHIFT_BITS;
+        depth = depth << DEPTH_SHIFT_BITS;
+        long typel = (long)(type.getIntValue()) << TYPE_SHIFT_BITS;
+
+        return offset | qname | depth | typel; 
+    }
 
     public static void main(String[] arg)
     {
@@ -122,6 +170,15 @@ public class VTDBits
         System.out.print(VTD_MAX_VALUE);
         System.out.print(" Mask:");
         System.out.println(Long.toHexString(VTD_MASK));
+
+        long vtd = packns(25,2,3,6,TokenType.TOKEN_STARTING_TAG);
+        System.out.print("SAMPLE VTD::");
+        System.out.println(Long.toHexString(vtd));
+        System.out.print("OFFSET::");
+        System.out.println(getOffset(vtd));
+        System.out.print("Type::");
+        System.out.println(getType(vtd));
+
 
 
     }
