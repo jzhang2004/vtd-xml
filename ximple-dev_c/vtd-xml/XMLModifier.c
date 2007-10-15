bbef7a42d62fbd6ed5534ccbd3ac5a9856e6e65b
jzhang2004 <jzhang2004>
2007-10-15 03:08:48 +0000

    added support for insert byte fragments and elementFragmentNs *
    modified output() and resetXMLModifier() and freeXMLModifier()

--- ximple-dev_c/vtd-xml/XMLModifier.c
+++ ximple-dev_c/vtd-xml/XMLModifier.c
@@ -16,16 +16,22 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */ 
 #include "XMLModifier.h"
+//#include "elementFragmentNs.h"
 
 static void check(XMLModifier *xm);
 static void sort(XMLModifier *xm);
 static void insertBytesAt(XMLModifier *xm, int offset, Long l);
-static void removeContent(XMLModifier *xm, int offset, int len);
+static void insertBytesAt2(XMLModifier *xm, int offset, Long lenPlusPointer);
+static void insertBytesAt3(XMLModifier *xm, int offset, ElementFragmentNs* ef);
+//static void removeContent(XMLModifier *xm, int offset, int len);
 static Long getBytes_UTF8(UCSChar *s);
 static Long getBytes_UTF16LE(UCSChar *s);
 static Long getBytes_UTF16BE(UCSChar *s);
 static Long getBytes_ISO_8859_1(UCSChar *s);
 static Long getBytes_ASCII(UCSChar *s);
+// Fwrite will make sure the entire segment of bytes gets written into the stream, 
+// otherwise it will return false
+
 
 static UByte *doubleCapacity(UByte *b, size_t cap){
 	b = realloc(b,cap);
@@ -34,6 +40,7 @@ static UByte *doubleCapacity(UByte *b, size_t cap){
 	}
 	return b;
 }
+/*convert UCSChar into bytes corresponding to UTF-8 encoding */
 static Long getBytes_UTF8(UCSChar *s){
 	/* < 0x7f  1 byte*/
 	/* < 0x7ff  2 bytes */
@@ -84,6 +91,7 @@ static Long getBytes_UTF8(UCSChar *s){
 	return ((Long)k<<32)|(Long)ba;
 }
 
+/*convert UCSChar into bytes corresponding to UTF-16LE encoding */
 static Long getBytes_UTF16LE(UCSChar *s){
 	size_t len = wcslen(s),i=0,k=0,capacity = max(len,8)<<1;/*minimum size is 16 */
 	int tmp,w1,w2;
@@ -115,6 +123,7 @@ static Long getBytes_UTF16LE(UCSChar *s){
 	return ((Long)k<<32)|(Long)ba;
 }
 
+/*convert UCSChar into bytes corresponding to UTF-16BE encoding */
 static Long getBytes_UTF16BE(UCSChar *s){
 	size_t len = wcslen(s),i=0,k=0,capacity = max(len,8)<<1;/*minimum size is 16 */
 	int tmp,w1,w2;
@@ -145,6 +154,7 @@ static Long getBytes_UTF16BE(UCSChar *s){
 	return ((Long)k<<32)|(Long)ba;
 }
 
+/*convert UCSChar into bytes corresponding to iso-8859-1 encoding */
 static Long getBytes_ISO_8859_1(UCSChar *s){
 	size_t len = wcslen(s),i=0;
 	UByte *ba = malloc(sizeof(UByte)*(len));
@@ -158,6 +168,7 @@ static Long getBytes_ISO_8859_1(UCSChar *s){
 	return ((Long)len<<32)|(Long)ba;
 }
 
+/*convert UCSChar into bytes corresponding to ascii encoding */
 static Long getBytes_ASCII(UCSChar *s){
 	size_t len = wcslen(s),i=0;
 	UByte *ba = malloc(sizeof(UByte)*(len));
@@ -171,7 +182,7 @@ static Long getBytes_ASCII(UCSChar *s){
 	return ((Long)len<<32)|(Long)ba;
 }
 
-
+/* create XMLModifier */ 
 
 XMLModifier *createXMLModifier(){
 	XMLModifier *xm = NULL;
@@ -204,19 +215,30 @@ XMLModifier *createXMLModifier2(VTDNav *vn){
 	bind4XMLModifier(xm, vn);
 	return xm;
 }
+/*  freeXMLModifier will remove */
 void freeXMLModifier(XMLModifier *xm){
 	if (xm!=NULL){
 		int i=0;
 		freeIntHash(xm->deleteHash);
 		freeIntHash(xm->insertHash);
-		freeFastLongBuffer(xm->flb);
+		
 		for (i=0;i<xm->fob->size;i++){
-			free((void *)lower32At(xm->fob,i));
+			Long l = longAt(xm->flb,i);
+			if ((l & (~0x1fffffffffffffffLL)) == MASK_INSERT_SEGMENT_BYTE
+				//|| (l & (~0x1fffffffffffffffLL)) == MASK_INSERT_SEGMENT_BYTE
+				)
+				free((void *)lower32At(xm->fob,i));
+			else if ((l & (~0x1fffffffffffffffLL)) == MASK_INSERT_FRAGMENT_NS){
+				freeElementFragmentNs((ElementFragmentNs *)lower32At(xm->fob,i));
+			}
 		}
+		freeFastLongBuffer(xm->flb);
 		freeFastLongBuffer(xm->fob);
 	}
 	free(xm);
 }
+
+/*  */
 void bind4XMLModifier(XMLModifier *xm, VTDNav *vn){
 	if (vn==NULL){
 		throwException2(invalid_argument,
@@ -273,6 +295,7 @@ void remove4XMLModifier(XMLModifier *xm){
 		removeToken(xm,i);
 	}
 }
+
 void removeToken(XMLModifier *xm, int i){
 	    int type = getTokenType(xm->md,i);
         int os,len;
@@ -301,6 +324,7 @@ void removeToken(XMLModifier *xm, int i){
         	    return;        	    
         }
 }
+
 void removeAttribute(XMLModifier *xm,int attrNameIndex){
 	int type = getTokenType(xm->md,attrNameIndex);
 	int os1,os2,len2;
@@ -316,6 +340,7 @@ void removeAttribute(XMLModifier *xm,int attrNameIndex){
 	else 
 		removeContent(xm,os1<<1,(os2+len2-os1+1)<<1); 
 }
+
 void removeContent(XMLModifier *xm, int offset, int len){
 	if (offset < xm->md->docOffset || len > xm->md->docLen 
 		|| offset + len > xm->md->docOffset + xm->md->docLen){
@@ -329,6 +354,7 @@ void removeContent(XMLModifier *xm, int offset, int len){
 	appendLong(xm->flb,((Long)len)<<32 | offset | MASK_DELETE);
 	appendLong(xm->fob,(Long)NULL);
 }
+
 void insertBytesAt(XMLModifier *xm, int offset, Long lenPlusPointer){
 	if (isUniqueIntHash(xm->insertHash,offset)==FALSE){
 		throwException2(modify_exception,
@@ -336,6 +362,24 @@ void insertBytesAt(XMLModifier *xm, int offset, Long lenPlusPointer){
 	}
 	appendLong(xm->flb, offset | MASK_INSERT_BYTE);
 	appendLong(xm->fob, lenPlusPointer);
+}
+
+void insertBytesAt2(XMLModifier *xm, int offset, Long lenPlusPointer){
+	if (isUniqueIntHash(xm->insertHash,offset)==FALSE){
+		throwException2(modify_exception,
+			"There can be only one insertion per offset value");
+	}
+	appendLong(xm->flb, offset | MASK_INSERT_SEGMENT_BYTE);
+	appendLong(xm->fob, lenPlusPointer);
+}
+
+void insertBytesAt3(XMLModifier *xm, int offset, ElementFragmentNs* ef){
+	if (isUniqueIntHash(xm->insertHash,offset)==FALSE){
+		throwException2(modify_exception,
+			"There can be only one insertion per offset value");
+		appendLong(xm->flb, offset | MASK_INSERT_FRAGMENT_NS);
+		appendLong(xm->fob, ef);
+	}
 }
 
 void updateToken(XMLModifier *xm, int index, UCSChar *newContent){
@@ -383,6 +427,7 @@ void insertAfterElement(XMLModifier *xm, UCSChar *s){
 	len = (int)(l>>32);
 	insertBytesAt(xm, offset+len, xm->gbytes(s));
 }
+
 void insertBeforeElement(XMLModifier *xm, UCSChar *s){
 	int startTagIndex = getCurrentIndex(xm->md);
 	int type =  getTokenType(xm->md, startTagIndex);
@@ -398,6 +443,7 @@ void insertBeforeElement(XMLModifier *xm, UCSChar *s){
 	else
 		insertBytesAt(xm,(offset)<<1,xm->gbytes(s));
 }
+
 void insertAttribute(XMLModifier *xm, UCSChar *attr){
 	int startTagIndex = getCurrentIndex(xm->md),offset,len;
 	int type = getTokenType(xm->md,startTagIndex);
@@ -413,6 +459,104 @@ void insertAttribute(XMLModifier *xm, UCSChar *attr){
 	else
 		insertBytesAt(xm, (offset+len)<<1,xm->gbytes(attr));
 }
+
+/* insert a byte array before the start of an element 
+   the byte array won't get freed when calling XMLModifier's 
+   resetXMLModifier() and freeXMLModifier() 
+*/
+void insertAfterElement2(XMLModifier *xm, UByte* ba, int arrayLen){
+	int startTagIndex = getCurrentIndex(xm->md);
+	int type =  getTokenType(xm->md, startTagIndex);
+	int offset, len;
+	Long l;
+	if (type!=TOKEN_STARTING_TAG){
+		throwException2(modify_exception,
+			"Token type is not a starting tag");
+	}
+	l = getElementFragment(xm->md);
+	offset = (int)l;
+	len = (int)(l>>32);
+	insertBytesAt2(xm, offset+len, (((Long)arrayLen)<<32)|(int)ba);
+}
+
+void insertBeforeElement2(XMLModifier *xm, UByte* ba, int arrayLen){
+	int startTagIndex = getCurrentIndex(xm->md);
+	int type =  getTokenType(xm->md, startTagIndex);
+	int offset;
+	if (type!=TOKEN_STARTING_TAG){
+		throwException2(modify_exception,
+			"Token type is not a starting tag");
+	}
+	offset = getTokenOffset(xm->md,startTagIndex)-1;
+	
+	if (xm->encoding < FORMAT_UTF_16BE)
+		insertBytesAt2(xm,offset, (((Long)arrayLen)<<32)|(int)ba);
+	else
+		insertBytesAt2(xm,(offset)<<1, (((Long)arrayLen)<<32)|(int)ba);
+}
+
+void insertAfterElement3(XMLModifier *xm, UByte* ba, int contentOffset, int contentLen){
+	int startTagIndex = getCurrentIndex(xm->md);
+	int type =  getTokenType(xm->md, startTagIndex);
+	int offset;
+	if (type!=TOKEN_STARTING_TAG){
+		throwException2(modify_exception,
+			"Token type is not a starting tag");
+	}
+	offset = getTokenOffset(xm->md,startTagIndex)-1;
+	
+	if (xm->encoding < FORMAT_UTF_16BE)
+		insertBytesAt2(xm,offset, (((Long)contentLen)<<32)|((int)ba+contentOffset));
+	else
+		insertBytesAt2(xm,(offset)<<1, (((Long)contentLen)<<32)|((int)ba+contentOffset));
+}
+
+void insertBeforeElement3(XMLModifier *xm, UByte* ba, int contentOffset, int contentLen){
+	int startTagIndex = getCurrentIndex(xm->md);
+	int type =  getTokenType(xm->md, startTagIndex);
+	int offset, len;
+	Long l;
+	if (type!=TOKEN_STARTING_TAG){
+		throwException2(modify_exception,
+			"Token type is not a starting tag");
+	}
+	l = getElementFragment(xm->md);
+	offset = (int)l;
+	len = (int)(l>>32);
+	insertBytesAt2(xm, offset+len, (((Long)contentLen)<<32)|((int)ba+contentOffset));
+}
+
+void insertBeforeElement4(XMLModifier *xm, ElementFragmentNs *ef){
+	int startTagIndex = getCurrentIndex(xm->md);
+	int type =  getTokenType(xm->md, startTagIndex);
+	int offset, len;
+	Long l;
+	if (type!=TOKEN_STARTING_TAG){
+		throwException2(modify_exception,
+			"Token type is not a starting tag");
+	}
+	l = getElementFragment(xm->md);
+	offset = (int)l;
+	len = (int)(l>>32);
+	insertBytesAt3(xm,offset+len,ef);
+}
+
+
+void insertAfterElement4(XMLModifier *xm, ElementFragmentNs *ef){
+	int startTagIndex = getCurrentIndex(xm->md);
+	int type =  getTokenType(xm->md, startTagIndex);
+	int offset;
+	if (type!=TOKEN_STARTING_TAG){
+		throwException2(modify_exception,
+			"Token type is not a starting tag");
+	}
+	offset = getTokenOffset(xm->md,startTagIndex)-1;
+	if (xm->encoding < FORMAT_UTF_16BE)
+		insertBytesAt3(xm,offset, ef);
+	else
+		insertBytesAt3(xm,(offset)<<1, ef);
+}
+
 
 /* the present implementation automatically garbage collect 
    the byte array newContentBytes when calling freeXMLModifier*/
@@ -500,6 +644,7 @@ void insertAttribute(XMLModifier *xm, UCSChar *attr){
 		insertBytesAt(xm, (offset+len)<<1,(((Long)l<<32) |(int)attrBytes));
 }*/
 
+/* write the updated document into a File */
 void output(XMLModifier *xm, FILE *f){
 	 Long l;
 	 size_t k;
@@ -508,8 +653,7 @@ void output(XMLModifier *xm, FILE *f){
 	if (f == NULL){
 		throwException2(invalid_argument,
 			"FILE can't be a NULL");
-	}
-           
+	}           
 	t = lower32At(xm->md->vtdBuffer,0);
 	start = (t==0)?
 		xm->md->docOffset:32;
@@ -536,13 +680,15 @@ void output(XMLModifier *xm, FILE *f){
 				inc = 1;
 			l = longAt(xm->flb,i);
 			if (inc == 1){                    
-				if ((l & (~0x1fffffffffffffffLL)) == XML_DELETE){
+				if ((l & (~0x1fffffffffffffffLL)) == MASK_DELETE){
 					t = lower32At(xm->flb,i);
 					k=fwrite(xm->md->XMLDoc+offset,sizeof(UByte),t-offset,f);
 					if (k!=t-offset)
 						throwException2(io_exception,"fwrite didn't complete");
 					offset = t + (upper32At(xm->flb,i) & 0x1fffffff);
-				}else { // insert
+				}else if ((l & (~0x1fffffffffffffffLL)) == MASK_INSERT_BYTE
+					|| (l & (~0x1fffffffffffffffLL)) == MASK_INSERT_SEGMENT_BYTE){ 
+					// insert
 					t = lower32At(xm->flb,i);
 					k=fwrite(xm->md->XMLDoc+offset,sizeof(UByte),t-offset,f);
 					if (k!=t-offset)
@@ -552,9 +698,16 @@ void output(XMLModifier *xm, FILE *f){
 					if (k!=t)
 						throwException2(io_exception,"fwrite didn't complete");                      
 					offset=lower32At(xm->flb,i);
+				}else {
+					t = lower32At(xm->flb,i);
+					k=fwrite(xm->md->XMLDoc+offset,sizeof(UByte),t-offset,f);
+					if (k!=t-offset)
+						throwException2(io_exception,"fwrite didn't complete");
+					writeFragmentToFile((ElementFragmentNs*)lower32At(xm->fob,i),f);
+					offset=lower32At(xm->flb,i);
 				}
 			} else {
-				if ((l & (~0x1fffffffffffffffLL)) == XML_DELETE){
+				if ((l & (~0x1fffffffffffffffLL)) == MASK_DELETE){
 					t = lower32At(xm->flb,i);
 					k=fwrite(xm->md->XMLDoc+offset,sizeof(UByte),t-offset,f);
 					if (k!=t-offset)
@@ -566,7 +719,8 @@ void output(XMLModifier *xm, FILE *f){
 					if (k!=t)
 						throwException2(io_exception,"fwrite didn't complete");  
 					offset = lower32At(xm->flb,i) + (upper32At(xm->flb,i) & 0x1fffffff);
-				}else{
+				}else if ((l & (~0x1fffffffffffffffLL)) == MASK_INSERT_BYTE
+					|| (l & (~0x1fffffffffffffffLL)) == MASK_INSERT_SEGMENT_BYTE){
 					t = lower32At(xm->flb,i+1);
 					k=fwrite(xm->md->XMLDoc+offset,sizeof(UByte),t-offset,f);
 					if (k!=t-offset)
@@ -577,7 +731,13 @@ void output(XMLModifier *xm, FILE *f){
 					if (k!=t)
 						throwException2(io_exception,"fwrite didn't complete");  
 					offset = lower32At(xm->flb,i+1) + (upper32At(xm->flb,i+1) & 0x1fffffff);
-				}                    
+				}else {
+					t = lower32At(xm->flb,i+1);
+					k=fwrite(xm->md->XMLDoc+offset,sizeof(UByte),t-offset,f);
+					if (k!=t-offset)
+						throwException2(io_exception,"fwrite didn't complete");
+					offset = lower32At(xm->flb,i+1) + (upper32At(xm->flb,i+1) & 0x1fffffff);
+				}
 			}
 		}  
 		t = start+len-offset;
@@ -634,18 +794,33 @@ static quickSort(XMLModifier* xm, int lo, int hi){
 	if (lo<j) quickSort(xm,lo, j);
 	if (i<hi) quickSort(xm,i, hi);
 }
+
+/* sort flb based on offset value so the sorted flb offset vals are document order*/
 static void sort(XMLModifier *xm){
 	if (xm->flb->size >0)
 		quickSort(xm,0,xm->flb->size-1);
 }
 
+/* resetXMLModifier */
 void resetXMLModifier(XMLModifier *xm){
 	if (xm != NULL){
+		if (xm->fob!= NULL){
+			int i=0;
+			for (i=0; i< xm->flb->size;i++){
+				// free byte segment and elementFragmentNs
+				Long l = longAt(xm->flb,i);
+				if ( (l & (~0x1fffffffffffffffLL)) == MASK_INSERT_BYTE
+					//||(l & (~0x1fffffffffffffffLL)) == MASK_INSERT_SEGMENT_BYTE
+					)
+					free((void *)lower32At(xm->fob,i));
+				else if ( (l & (~0x1fffffffffffffffLL)) == MASK_INSERT_FRAGMENT_NS){
+					freeElementFragmentNs((ElementFragmentNs *)lower32At(xm->fob,i));
+				}
+			}
+			clearFastLongBuffer(xm->fob);
+		}
 		if (xm->flb!= NULL){
 			clearFastLongBuffer(xm->flb);
-		}
-		if (xm->fob!= NULL){
-			clearFastLongBuffer(xm->fob);
 		}
 		if (xm->insertHash!= NULL){
 			resetIntHash(xm->insertHash);
@@ -655,3 +830,15 @@ void resetXMLModifier(XMLModifier *xm){
 		}
 	}
 }
+/* 
+ByteSegment* createByteSegment(){
+	ByteSegment* bs = NULL;
+	return bs;
+}
+void freeByteSegment(ByteSegment *bs){
+	free(bs);
+}
+*/
+
+
+
