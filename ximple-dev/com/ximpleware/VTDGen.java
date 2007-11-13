52a58da01610bc89672ac5dd296f8e3b42e12e4d
jzhang2004 <jzhang2004>
2007-11-13 21:53:04 +0000

    added zero length character data token support ie <a></a> now will have a zero-length text node

--- ximple-dev/com/ximpleware/VTDGen.java
+++ ximple-dev/com/ximpleware/VTDGen.java
@@ -1760,7 +1760,7 @@ public class VTDGen {
 									if (helper == true) {
 										length1 = offset - temp_offset
 												- (increment << 1);
-										if (length1 > 0) {
+										//if (length1 > 0) {
 											if (encoding < FORMAT_UTF_16BE)
 												writeVTD((temp_offset),
 														length1,
@@ -1771,7 +1771,7 @@ public class VTDGen {
 														(length1 >> 1),
 														TOKEN_CHARACTER_DATA,
 														depth);
-										}
+										//}
 									}
 									parser_state = STATE_END_TAG;
 									break;
