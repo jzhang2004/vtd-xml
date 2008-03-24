7ce8d07593c9c1916aef499c5db73245433d2e7c
jzhang2004 <jzhang2004>
2008-03-24 00:11:06 +0000

    get AutoPilot up to date fix iterAttr bug

--- vtd-xml-HUGE/java/com/ximpleware/AutoPilot.java
+++ vtd-xml-HUGE/java/com/ximpleware/AutoPilot.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2002-2007 XimpleWare, info@ximpleware.com
+ * Copyright (C) 2002-2008 XimpleWare, info@ximpleware.com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,22 +21,10 @@ package com.ximpleware;
 import com.ximpleware.xpath.*;
 import java.util.*;
 import java.io.*;
-/* 
- * Copyright (C) 2002-2008 XimpleWare, info@ximpleware.com
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+/**
+ * XimpleWare's AutoPilot implementation encapsulating node iterator
+ * and XPath.
+ * 
  */
 public class AutoPilot {
     private int depth;
@@ -277,8 +265,9 @@ public boolean iterate() throws PilotException, NavException {
    	    				}
    	    				return -1;
    	    			}else {
-   	    				int type = vn.getTokenType(index);
+   	    				
    	    				while(index<=size){
+   	    				 int type = vn.getTokenType(index);
 	    					if (type == VTDNav.TOKEN_ATTR_NAME
 	    						|| type == VTDNav.TOKEN_ATTR_NS){
 	    						if (type == VTDNav.TOKEN_ATTR_NAME){
@@ -290,6 +279,7 @@ public boolean iterate() throws PilotException, NavException {
 	    					}else{   	    				
 	    						return -1;
 	    					}
+	    					
    	    				}
    	    				return -1;
    	    			}
