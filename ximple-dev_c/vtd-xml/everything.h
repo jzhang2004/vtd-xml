7998d7fab1cf93264407e507da0eb72ad971756b
jzhang2004 <jzhang2004>
2008-03-30 21:06:01 +0000

    added textIter.h

--- ximple-dev_c/vtd-xml/everything.h
+++ ximple-dev_c/vtd-xml/everything.h
@@ -1,3 +1,25 @@
+/* 
+ * Copyright (C) 2002-2008 XimpleWare, info@ximpleware.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+/* this file decodes various functions and data structures that conver
+   encodings format into Unicode */
+
+#ifndef EVEVYTHING_H
+#define EVERYTHING_H
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,3 +35,5 @@
 #include "XMLModifier.h"
 #include "nodeRecorder.h"
 #include "bookMark.h"
+#include "textIter.h"
+#endif 
