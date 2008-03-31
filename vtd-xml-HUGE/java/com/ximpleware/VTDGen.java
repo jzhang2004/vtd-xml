fd5fd05bb9a9285539d392b5872600213fa8970e
jzhang2004 <jzhang2004>
2008-03-31 18:41:37 +0000

    fixed the text after cdata problem

--- vtd-xml-HUGE/java/com/ximpleware/VTDGen.java
+++ vtd-xml-HUGE/java/com/ximpleware/VTDGen.java
@@ -2870,15 +2870,16 @@ public class VTDGen {
 		if (ch == '<') {
 			parser_state = STATE_LT_SEEN;
 		} else if (XMLChar.isContentChar(ch)) {
-			temp_offset = offset;
+			temp_offset = offset-1;
 			parser_state = STATE_TEXT;
 		} else if (ch == '&') {
 			//has_amp = true;
-			temp_offset = offset;
+			temp_offset = offset-1;
 			entityIdentifier();
 			parser_state = STATE_TEXT;
 			//temp_offset = offset;
 		} else if (ch == ']') {
+		    temp_offset = offset-1;
 			if (r.skipChar(']')) {
 				while (r.skipChar(']')) {
 				}
