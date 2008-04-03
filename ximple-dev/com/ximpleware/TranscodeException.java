88e86d4b963b0346e4d5373d9f59b701a89fd26e
jzhang2004 <jzhang2004>
2008-04-03 00:29:24 +0000

    comments added

--- ximple-dev/com/ximpleware/TranscodeException.java
+++ ximple-dev/com/ximpleware/TranscodeException.java
@@ -15,8 +15,13 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
-package com.ximpleware;
+package com.ximpleware; 
 
+/**
+ * TranscodeException is thrown when there is exception condition (in XMLModifier)
+ * for transcoding characters 
+ * 
+ */
 public class TranscodeException extends VTDException {
     public TranscodeException(String s){
 		super(s);
