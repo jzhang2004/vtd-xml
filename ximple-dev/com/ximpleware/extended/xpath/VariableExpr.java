e09f81ed0fe9013f99997eb001d2991444cafb4c
jzhang2004 <jzhang2004>
2009-09-22 20:37:05 +0000

    remove @override

--- ximple-dev/com/ximpleware/extended/xpath/VariableExpr.java
+++ ximple-dev/com/ximpleware/extended/xpath/VariableExpr.java
@@ -16,85 +16,84 @@ public class VariableExpr extends Expr {
 		exprVal = e;
 	}
 	
-	@Override
+	
 	public int adjust(int n) {
 		// TODO Auto-generated method stub
 		return exprVal.adjust(n);
 	}
 
-	@Override
+	
 	public boolean evalBoolean(VTDNavHuge vn) {
 		// TODO Auto-generated method stub
 		return exprVal.evalBoolean(vn);
 	}
 
-	@Override
+	
 	public int evalNodeSet(VTDNavHuge vn) throws XPathEvalExceptionHuge, NavExceptionHuge {
 		// TODO Auto-generated method stub
 		return exprVal.evalNodeSet(vn);
 	}
 
-	@Override
+	
 	public double evalNumber(VTDNavHuge vn) {
 		// TODO Auto-generated method stub
 		return exprVal.evalNumber(vn);
 	}
 
-	@Override
 	public String evalString(VTDNavHuge vn) {
 		// TODO Auto-generated method stub
 		return exprVal.evalString(vn);
 	}
 
-	@Override
+	
 	public boolean isBoolean() {
 		// TODO Auto-generated method stub
 		return exprVal.isBoolean();
 	}
 
-	@Override
+	
 	public boolean isNodeSet() {
 		// TODO Auto-generated method stub
 		return exprVal.isNodeSet();
 	}
 
-	@Override
+	
 	public boolean isNumerical() {
 		// TODO Auto-generated method stub
 		return exprVal.isNumerical();
 	}
 
-	@Override
+	
 	public boolean isString() {
 		// TODO Auto-generated method stub
 		return exprVal.isString();
 	}
 
-	@Override
+	
 	public boolean requireContextSize() {
 		// TODO Auto-generated method stub
 		return exprVal.requireContextSize();
 	}
 
-	@Override
+	
 	public void reset(VTDNavHuge vn) {
 		// TODO Auto-generated method stub
 		exprVal.reset(vn);
 	}
 
-	@Override
+	
 	public void setContextSize(int size) {
 		// TODO Auto-generated method stub
 		exprVal.setContextSize(size);
 	}
 
-	@Override
+	
 	public void setPosition(int pos) {
 		// TODO Auto-generated method stub
 		exprVal.setPosition(pos);
 	}
 
-	@Override
+	
 	public String toString() {
 		// TODO Auto-generated method stub
 		return "$"+exprName;
