caa6021c680863a8bc220c04d3acadf4cb160483
jzhang2004 <jzhang2004>
2009-10-20 22:05:22 +0000

    imported packages name change

--- ximple-dev/com/ximpleware/xpath/VariableExpr.java
+++ ximple-dev/com/ximpleware/xpath/VariableExpr.java
@@ -16,10 +16,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 package com.ximpleware.xpath;
-
-import com.ximpleware.NavException;
-import com.ximpleware.VTDNav;
-import com.ximpleware.XPathEvalException;
+import com.ximpleware.*;
 
 public class VariableExpr extends Expr {
 	
