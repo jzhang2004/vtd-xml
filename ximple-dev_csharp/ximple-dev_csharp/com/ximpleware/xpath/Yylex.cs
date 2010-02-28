01b4556b0bdda6aadbae870048657f55175d0022
jzhang2004 <jzhang2004>
2010-02-28 05:20:14 +0000

    changed copyrighted year to 2010

--- ximple-dev_csharp/ximple-dev_csharp/com/ximpleware/xpath/Yylex.cs
+++ ximple-dev_csharp/ximple-dev_csharp/com/ximpleware/xpath/Yylex.cs
@@ -2,7 +2,7 @@
 
 #line 1 "scanner_cs.flex"
 /* 
- * Copyright (C) 2002-2009 XimpleWare, info@ximpleware.com
+ * Copyright (C) 2002-2010 XimpleWare, info@ximpleware.com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
