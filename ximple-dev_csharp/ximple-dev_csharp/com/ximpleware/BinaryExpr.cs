d7bae21e8a9af7d7c7e43b6d39ac7ca5ca672df5
jzhang2004 <jzhang2004>
2010-03-03 01:49:51 +0000

    Expr changed to com.ximpleware from com.ximpleware.xpath

--- ximple-dev_csharp/ximple-dev_csharp/com/ximpleware/BinaryExpr.cs
+++ ximple-dev_csharp/ximple-dev_csharp/com/ximpleware/BinaryExpr.cs
@@ -16,7 +16,7 @@
 * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 using System;
-using Expr = com.ximpleware.xpath.Expr;
+using Expr = com.ximpleware.Expr;
 using XPathEvalException = com.ximpleware.XPathEvalException;
 namespace com.ximpleware
 {
