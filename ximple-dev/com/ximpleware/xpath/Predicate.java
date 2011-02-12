bf2bb01b544420880620b61fedb9066c97f5d49b
jzhang2004 <jzhang2004>
2011-02-12 03:25:05 +0000

    add adjust(int n);

--- ximple-dev/com/ximpleware/xpath/Predicate.java
+++ ximple-dev/com/ximpleware/xpath/Predicate.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2002-2010 XimpleWare, info@ximpleware.com
+ * Copyright (C) 2002-2011 XimpleWare, info@ximpleware.com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -73,6 +73,10 @@ public class Predicate implements LocationPathNode{
 	
 	public void setContextSize(int size){
 	    expr.setContextSize(size);
+	}
+	
+	public void adjust(int n){
+		expr.adjust(n);
 	}
 
 }
