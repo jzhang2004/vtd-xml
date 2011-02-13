f5f4d73225242527f4176c68cba92ed8832b64cd
jzhang2004 <jzhang2004>
2011-02-13 21:05:06 +0000

    added support for 5 level LC

--- ximple-dev_c/vtd-xml/bookMark.c
+++ ximple-dev_c/vtd-xml/bookMark.c
@@ -55,14 +55,27 @@ void bind4BookMark(BookMark *bm, VTDNav *vn){
 		if (vn==NULL)
             throwException2(invalid_argument,"vn can't be null");
         bm->vn1 = vn;
-		if (bm->ba == NULL || vn->nestingLevel+8 != bm->ba_len){
-			if (vn->nestingLevel+8 != bm->ba_len){
-				free(bm->ba);
+		if (vn->shallowDepth){
+			if (bm->ba == NULL || vn->nestingLevel+8 != bm->ba_len){
+				if (vn->nestingLevel+8 != bm->ba_len){
+					free(bm->ba);
+				}
+				bm->ba = malloc(sizeof(int)*(vn->nestingLevel + 8)); 
+				if (bm->ba == NULL){
+					throwException2(out_of_mem,
+						"BookMark.ba allocation failed ");
+				}
 			}
-            bm->ba = malloc(sizeof(int)*(vn->nestingLevel + 8)); 
-			if (bm->ba == NULL){
-				throwException2(out_of_mem,
-			"BookMark.ba allocation failed ");
+		}else{
+			if (bm->ba == NULL || vn->nestingLevel+8 != bm->ba_len){
+				if (vn->nestingLevel+14 != bm->ba_len){
+					free(bm->ba);
+				}
+				bm->ba = malloc(sizeof(int)*(vn->nestingLevel + 14)); 
+				if (bm->ba == NULL){
+					throwException2(out_of_mem,
+						"BookMark.ba allocation failed ");
+				}
 			}
 		}
         bm->ba[0]= -2 ; // this would never happen in a VTDNav obj's context
@@ -78,19 +91,42 @@ Boolean setCursorPosition(BookMark *bm, VTDNav *vn){
 		vn->context[i] = bm->ba[i];
 	}
 
-	vn->l1index = bm->ba[vn->nestingLevel];
-	vn->l2index = bm->ba[vn->nestingLevel + 1];
-	vn->l3index = bm->ba[vn->nestingLevel + 2];
-	vn->l2lower = bm->ba[vn->nestingLevel + 3];
-	vn->l2upper = bm->ba[vn->nestingLevel + 4];
-	vn->l3lower = bm->ba[vn->nestingLevel + 5];
-	vn->l3upper = bm->ba[vn->nestingLevel + 6];
-	if (bm->ba[vn->nestingLevel+7] < 0){
-		vn->atTerminal = TRUE;		    
-	} else
-		vn->atTerminal = FALSE;
+	if (vn->shallowDepth){
+		vn->l1index = bm->ba[vn->nestingLevel];
+		vn->l2index = bm->ba[vn->nestingLevel + 1];
+		vn->l3index = bm->ba[vn->nestingLevel + 2];
+		vn->l2lower = bm->ba[vn->nestingLevel + 3];
+		vn->l2upper = bm->ba[vn->nestingLevel + 4];
+		vn->l3lower = bm->ba[vn->nestingLevel + 5];
+		vn->l3upper = bm->ba[vn->nestingLevel + 6];
+		if (bm->ba[vn->nestingLevel+7] < 0){
+			vn->atTerminal = TRUE;		    
+		} else
+			vn->atTerminal = FALSE;
 
-	vn->LN = bm->ba[vn->nestingLevel+7] & 0x7fffffff;
+		vn->LN = bm->ba[vn->nestingLevel+7] & 0x7fffffff;
+	}else{
+		VTDNav_L5 *vnl = (VTDNav_L5 *)vn;
+		vnl->l1index = bm->ba[vn->nestingLevel];
+		vnl->l2index = bm->ba[vn->nestingLevel + 1];
+		vnl->l3index = bm->ba[vn->nestingLevel + 2];
+		vnl->l4index = bm->ba[vn->nestingLevel + 3];
+		vnl->l5index = bm->ba[vn->nestingLevel + 4];
+		vnl->l2lower = bm->ba[vn->nestingLevel + 5];
+		vnl->l2upper = bm->ba[vn->nestingLevel + 6];
+		vnl->l3lower = bm->ba[vn->nestingLevel + 7];
+		vnl->l3upper = bm->ba[vn->nestingLevel + 8];
+		vnl->l4lower = bm->ba[vn->nestingLevel + 9];
+		vnl->l4upper = bm->ba[vn->nestingLevel + 10];
+		vnl->l4lower = bm->ba[vn->nestingLevel + 11];
+		vnl->l4upper = bm->ba[vn->nestingLevel + 12];
+		if (bm->ba[vn->nestingLevel+13] < 0){
+			vn->atTerminal = TRUE;		    
+		} else
+			vn->atTerminal = FALSE;
+
+		vn->LN = bm->ba[vn->nestingLevel+13] & 0x7fffffff;
+	}
 	return TRUE;
 }
 Boolean setCursorPosition2(BookMark *bm){
@@ -114,17 +150,39 @@ Boolean recordCursorPosition(BookMark *bm, VTDNav *vn){
 		bm->ba[i] = bm->vn1->context[i];
 	}
 
-	bm->ba[vn->nestingLevel]= vn->l1index ;
-	bm->ba[vn->nestingLevel + 1]= vn->l2index ;
-	bm->ba[vn->nestingLevel + 2]= vn->l3index ;
-	bm->ba[vn->nestingLevel + 3]= vn->l2lower ;
-	bm->ba[vn->nestingLevel + 4]= vn->l2upper ;
-	bm->ba[vn->nestingLevel + 5]= vn->l3lower ;
-	bm->ba[vn->nestingLevel + 6]= vn->l3upper ;
-	//ba[vn.nestingLevel + 7]=(vn.atTerminal == true)?1:0;
-	bm->ba[vn->nestingLevel + 7]= 
-		(vn->atTerminal == TRUE)? 
-		(vn->LN | 0x80000000) : vn->LN ;
+	if (vn->shallowDepth){
+		bm->ba[vn->nestingLevel]= vn->l1index ;
+		bm->ba[vn->nestingLevel + 1]= vn->l2index ;
+		bm->ba[vn->nestingLevel + 2]= vn->l3index ;
+		bm->ba[vn->nestingLevel + 3]= vn->l2lower ;
+		bm->ba[vn->nestingLevel + 4]= vn->l2upper ;
+		bm->ba[vn->nestingLevel + 5]= vn->l3lower ;
+		bm->ba[vn->nestingLevel + 6]= vn->l3upper ;
+		//ba[vn.nestingLevel + 7]=(vn.atTerminal == true)?1:0;
+		bm->ba[vn->nestingLevel + 7]= 
+			(vn->atTerminal == TRUE)? 
+			(vn->LN | 0x80000000) : vn->LN ;
+	}
+	else{
+		VTDNav_L5 *vnl = (VTDNav_L5 *)vn;
+		bm->ba[vn->nestingLevel]= vnl->l1index ;
+		bm->ba[vn->nestingLevel + 1]= vnl->l2index ;
+		bm->ba[vn->nestingLevel + 2]= vnl->l3index ;
+		bm->ba[vn->nestingLevel + 3]= vnl->l4index ;
+		bm->ba[vn->nestingLevel + 4]= vnl->l5index ;
+		bm->ba[vn->nestingLevel + 5]= vnl->l2lower ;
+		bm->ba[vn->nestingLevel + 6]= vnl->l2upper ;
+		bm->ba[vn->nestingLevel + 7]= vnl->l3lower ;
+		bm->ba[vn->nestingLevel + 8]= vnl->l3upper ;
+		bm->ba[vn->nestingLevel + 9]= vnl->l4lower ;
+		bm->ba[vn->nestingLevel + 10]= vnl->l4upper ;
+		bm->ba[vn->nestingLevel + 11]= vnl->l5lower ;
+		bm->ba[vn->nestingLevel + 12]= vnl->l5upper ;
+		bm->ba[vn->nestingLevel + 13]= 
+			(vn->atTerminal == TRUE)? 
+			(vn->LN | 0x80000000) : vn->LN ;
+
+	}
 	return TRUE;
 }
 
@@ -165,10 +223,18 @@ int hashCode4BookMark(BookMark *bm){
 Boolean deepEqual4BookMark(BookMark *bm1, BookMark *bm2){
 	if (bm2->vn1 == bm1->vn1){
 		if (bm2->ba[bm2->ba[0]]==bm1->ba[bm1->ba[0]]){
-			if (bm1->ba[bm1->vn1->nestingLevel+7] < 0){
-				if (bm1->ba[bm1->vn1->nestingLevel+7]
-				!= bm2->ba[bm1->vn1->nestingLevel+7])
-					return FALSE;
+			if (bm1->vn1->shallowDepth){
+				if (bm1->ba[bm1->vn1->nestingLevel+7] < 0){
+					if (bm1->ba[bm1->vn1->nestingLevel+7]
+					!= bm2->ba[bm1->vn1->nestingLevel+7])
+						return FALSE;
+				}
+			}else{
+				if (bm1->ba[bm1->vn1->nestingLevel+13] < 0){
+					if (bm1->ba[bm1->vn1->nestingLevel+13]
+					!= bm2->ba[bm1->vn1->nestingLevel+13])
+						return FALSE;
+				}
 			}
 			return TRUE;
 		}
