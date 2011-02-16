70e845009e6c8627e77f5416020049e795a9ea01
jzhang2004 <jzhang2004>
2011-02-16 20:02:49 +0000

    inclusion bug fix from VTDNav_L5 to vtdNav_L5

--- ximple-dev_cpp/vtd-xml/bookMark.cpp
+++ ximple-dev_cpp/vtd-xml/bookMark.cpp
@@ -16,7 +16,7 @@
 * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 #include "bookMark.h"
-#include "VTDNav_L5.h"
+#include "vtdNav_L5.h"
 using namespace com_ximpleware;
 
 BookMark::BookMark():
