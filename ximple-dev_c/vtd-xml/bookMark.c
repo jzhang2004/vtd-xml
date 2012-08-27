2e6d62ae870d240988c203b7909bc77725264bd4
jzhang2004 <jzhang2004>
2012-08-27 01:16:58 +0000

    copyright year change

--- ximple-dev_c/vtd-xml/bookMark.c
+++ ximple-dev_c/vtd-xml/bookMark.c
@@ -1,5 +1,5 @@
 /* 
-* Copyright (C) 2002-2010 XimpleWare, info@ximpleware.com
+* Copyright (C) 2002-2012 XimpleWare, info@ximpleware.com
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -16,6 +16,7 @@
 * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 #include "bookMark.h"
+#include "vtdNav.h"
 
 BookMark *createBookMark(){
 	BookMark *bm = (BookMark*) malloc(sizeof(BookMark));
