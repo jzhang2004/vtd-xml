2590b68c0576182c9739387ab72c7d9c87810bca
jzhang2004 <jzhang2004>
2012-09-06 01:19:49 +0000

    head inclusion bug fix

--- ximple-dev_c/vtd-xml/funcExpr.c
+++ ximple-dev_c/vtd-xml/funcExpr.c
@@ -16,7 +16,7 @@
 * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 #include "xpath.h"
-#include "xmlModifier.h"
+#include "XMLModifier.h"
 
 static double sum(funcExpr *fne, VTDNav *vn);
 static int argCount(funcExpr *fne);
