03c7739d241d22188d66ecfef303a70628b51c91
jzhang2004 <jzhang2004>
2013-07-24 01:38:46 +0000

    copy right year change to 2013 added patent clause removed getNav()

--- ximple-dev/com/ximpleware/XSLPilot.java
+++ ximple-dev/com/ximpleware/XSLPilot.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2002-2012 XimpleWare, info@ximpleware.com
+ * Copyright (C) 2002-2013 XimpleWare, info@ximpleware.com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,6 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
+/*VTD-XML is protected by US patent 7133857, 7260652, an 7761459*/
 package com.ximpleware;
 
 import java.io.StringReader;
@@ -25,7 +26,7 @@ import com.ximpleware.xpath.XSLParser;
 public class XSLPilot extends AutoPilot {	
 	static private Hashtable symbolHash;
 	
-	public VTDNav getNav(){return this.vn;}
+	//public VTDNav getNav(){return this.vn;}
 	
 	public void selectXslXPath(String s) throws XPathParseException {
 	    try{
