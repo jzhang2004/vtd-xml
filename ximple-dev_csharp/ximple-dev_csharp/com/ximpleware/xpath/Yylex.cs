cb93f429a32efc8606cabf341219a11f26c017d9
jzhang2004 <jzhang2004>
2013-07-27 01:25:34 +0000

    2013 copy right year change, patent notice added

--- ximple-dev_csharp/ximple-dev_csharp/com/ximpleware/xpath/Yylex.cs
+++ ximple-dev_csharp/ximple-dev_csharp/com/ximpleware/xpath/Yylex.cs
@@ -2,7 +2,7 @@
 
 #line 1 "scanner_cs.flex"
 /* 
- * Copyright (C) 2002-2012 XimpleWare, info@ximpleware.com
+ * Copyright (C) 2002-2013 XimpleWare, info@ximpleware.com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,7 +18,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
-
+/*VTD-XML is protected by US patent 7133857, 7260652, an 7761459*/
 namespace com.ximpleware.xpath {
 
 
