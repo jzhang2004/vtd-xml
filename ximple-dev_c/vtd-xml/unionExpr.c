e4a998a00e91e56c6a46c6b3b1846bc7628d1236
jzhang2004 <jzhang@ximpleware.com>
2016-05-19 20:46:31 -0700

    bug fix in evalString

--- ximple-dev_c/vtd-xml/unionExpr.c
+++ ximple-dev_c/vtd-xml/unionExpr.c
@@ -212,7 +212,7 @@ UCSChar* evalString_une  (unionExpr *une,VTDNav *vn){
                 switch(t){
 			 case TOKEN_STARTING_TAG:
 			 case TOKEN_DOCUMENT:
-				 s = getXPathStringVal(vn,0);
+				 s = getXPathStringVal2(vn,a,0);
 				 break;
 			 case TOKEN_ATTR_NAME:
 				 s = toString(vn,a + 1);
